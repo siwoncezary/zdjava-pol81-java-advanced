75548603bdf65929ea256eab1808b5bf5214c8b9
cs <siwon.cezary@gmail.com>
2021-05-10 20:13:35 +0200

    end of day 2

--- src/main/java/pl/sda/JsonApp.java
+++ src/main/java/pl/sda/JsonApp.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -24,48 +25,76 @@ public class JsonApp extends Application {
         VBox root = new VBox();
         root.setPadding(new Insets(10));
         root.setSpacing(10);
-        TextArea jsonView = new TextArea();
-        Button createBtn = new Button("Create");
-        Button jsonBtn = new Button("To JSON");
-        TextField result = new TextField();
+        TextArea jsonView = new TextArea("{\n" +
+                "\t\"name\": \"ALA\",\n" +
+                "\t\"points\": 123,\n" +
+                "\t\"isActive\": true,\n" +
+                "\t\"games\":[\"AA\",\"BB\",\"CC\"],\n" +
+                "\t\"address\":{\n" +
+                "\t\t\"city\":\"Warsaw\",\n" +
+                "\t\t\"zip\": 23456\n" +
+                "\t}\n" +
+                "}");
+        jsonView.setEditable(false);
+        Button treeJsonBtn = new Button("Parse tree JSON");
+        Button valueJsonBtn = new Button("Parse JSON to class");
+        Button jsonBtn = new Button("Class to JSON");
+        TextArea result = new TextArea();
+
         jsonBtn.setOnAction(e -> {
-           Player player = new Player("KAROL", 100, true, new String[]{"BBB","FFF"});
-           PlayerDto playerDto = new PlayerDto(player);
-           ObjectMapper mapper = new ObjectMapper();
+            Player player = new Player("KAROL", 100, true, new String[]{"BBB", "FFF"});
+            PlayerDto playerDto = new PlayerDto(player);
+            Alert info = new Alert(Alert.AlertType.INFORMATION);
+            info.setHeaderText("Konwersja obiektu PlayerDto do łańcucha JSON.");
+            info.setContentText(player.toString());
+            info.show();
+            ObjectMapper mapper = new ObjectMapper();
             try {
                 //Zamiana obiektu na JSON w łańcuchu
                 String json = mapper.writeValueAsString(playerDto);
-                jsonView.setText(json);
+                result.setText(json);
             } catch (JsonProcessingException jsonProcessingException) {
                 jsonProcessingException.printStackTrace();
             }
         });
-        createBtn.setOnAction(e ->{
-            ObjectMapper mapper= new ObjectMapper();
+
+        valueJsonBtn.setOnAction(e -> {
+            ObjectMapper mapper = new ObjectMapper();
             try {
                 //mapowanie całego Json na klasę PLayer
                 Player player = mapper.readValue(jsonView.getText(), Player.class);
-//                JsonNode node = mapper.readTree(jsonView.getText());
-//                int zip = node.get("address").get("zip").asInt();
-//                int points = node.get("points").asInt();
-//                String name = node.get("name").asText();
-//                JsonNode games = node.get("games");
-//                List<String> gamesList = new ArrayList<>();
-//                if (games.isArray()){
-//                    for(JsonNode game: games){
-//                        gamesList.add(game.asText());
-//                    }
-//                }
-//                boolean isActive = node.get("isActive").asBoolean();
-//                result.setText("points " + points + " name " + name + " isActive " + isActive +" games " + gamesList +" zip "+ zip);
                 result.setText(player.toString());
             } catch (JsonProcessingException jsonProcessingException) {
                 jsonProcessingException.printStackTrace();
             }
         });
 
-        root.getChildren().addAll(jsonView, createBtn, jsonBtn, result);
-        Scene scene = new Scene(root, 400,600);
+        treeJsonBtn.setOnAction(e -> {
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode node = null;
+            try {
+                //Odczytanie Json jako drzewa węzłów, node jest korzeniem drzewa
+                node = mapper.readTree(jsonView.getText());
+            } catch (JsonProcessingException jsonProcessingException) {
+                jsonProcessingException.printStackTrace();
+            }
+            //odczyt bezpośrednich potomków węzła głównego
+            int zip = node.get("address").get("zip").asInt();
+            int points = node.get("points").asInt();
+            String name = node.get("name").asText();
+            JsonNode games = node.get("games");
+            List<String> gamesList = new ArrayList<>();
+            if (games.isArray()) {
+                for (JsonNode game : games) {
+                    gamesList.add(game.asText());
+                }
+            }
+            boolean isActive = node.get("isActive").asBoolean();
+            result.setText("points " + points + "\nname " + name + "\nisActive " + isActive + "\ngames " + gamesList + "\nzip " + zip);
+        });
+
+        root.getChildren().addAll(jsonView, treeJsonBtn, valueJsonBtn, jsonBtn, result);
+        Scene scene = new Scene(root, 400, 600);
         stage.setScene(scene);
         stage.show();
     }
